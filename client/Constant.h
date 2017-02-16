a6e9c7d0748582ce45b47b42701066862c8c3cc2
githubbenjamin <yaoze189@yeah.net>
2017-02-16 10:40:54 +0800

    Create Constant.h

---
+++ client/Constant.h
@@ -0,0 +1,22 @@
+//
+//  constant.h
+//  OTFClient
+//
+//  Created by gdlocal on 12/5/15.
+//  Copyright (c) 2015 gdlocal. All rights reserved.
+//
+
+#ifndef OTFClient_constant_h
+#define OTFClient_constant_h
+
+enum  CommuType{
+    MSG_SEND = 0,
+    MSG_READ = 1
+    };
+
+#define NOTIFICATION_USER_INFO_MESSAGE_KEY @"MESSAGE"
+#define NOTIFICATION_USER_INFO_MESSAGE_TYPE_KEY @"MESSAGE_TYPE"
+
+#define NOTIFICATION_SEND_RECEIVE_MESSAGE_TYPE @"SEND_RECEIVE_MESSAGE"
+
+#endif
